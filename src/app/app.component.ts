1119258f9467b9e9ac88da6a80dc4ac18257d740
Ricardo Belfort <ricardo.belfort.magnotech@gmgroupbrasil.com.br>
2025-07-01 19:54:57 -0400

    fix: ajustando componentes visiveis para paginas publicas e privadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,57 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { LoadingComponent } from '@shared/components/loading/loading.component';
 import { PrimeNG } from 'primeng/config';
 import { ToastModule } from 'primeng/toast';
+import { filter } from 'rxjs';
 import { supabase } from 'supabase.client';
 import { APP_VERSION } from '../version';
 import { MenubarComponent } from './shared/components/menubar/menubar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToastModule, LoadingComponent, MenubarComponent],
+  imports: [CommonModule, RouterOutlet, ToastModule, LoadingComponent, MenubarComponent],
   standalone: true,
   template: `
     <div class="w-full flex flex-col min-h-screen">
-      <app-menubar></app-menubar>
-      <p-toast [showTransitionOptions]="'500ms'" [hideTransitionOptions]="'500ms'" position="top-right" />
-      <app-loading />
-      <main class="flex-1 max-w-[1280px] mx-auto px-4 w-full">
+      <ng-container *ngIf="!isPublicPage">
+        <app-menubar></app-menubar>
+        <p-toast [showTransitionOptions]="'500ms'" [hideTransitionOptions]="'500ms'" position="top-right" />
+        <app-loading />
+      </ng-container>
+      <main
+        [class]="
+          isPublicPage ? 'flex-1 flex flex-col justify-center items-center bg-[#f7f9fc]' : 'max-w-[1280px] mx-auto px-4'
+        "
+      >
         <router-outlet />
       </main>
-      <footer
-        class="w-full bg-gray-50 border-t border-gray-200 py-4 px-6 flex flex-col md:flex-row items-center justify-between text-xs text-gray-500 gap-2"
-      >
-        <div>
-          <span class="font-semibold text-gray-700">BoostManager</span>
-          &copy; {{ currentYear }}. Todos os direitos reservados.
-        </div>
-        <div>
-          Versão <span class="font-mono bg-gray-100 rounded px-2 py-0.5">{{ APP_VERSION }}</span> | Desenvolvido por
-          <a href="https://belfortweb.com.br" target="_blank" class="underline hover:text-green-500">Ricardo Belfort</a>
-        </div>
-      </footer>
+      <ng-container *ngIf="!isPublicPage">
+        <footer
+          class="w-full bg-gray-50 border-t border-gray-200 py-4 px-6 flex flex-col md:flex-row items-center justify-between text-xs text-gray-500 gap-2"
+        >
+          <div>
+            <span class="font-semibold text-gray-700">BoostManager</span>
+            &copy; {{ currentYear }}. Todos os direitos reservados.
+          </div>
+          <div>
+            Versão <span class="font-mono bg-gray-100 rounded px-2 py-0.5">{{ APP_VERSION }}</span> | Desenvolvido por
+            <a href="https://belfortweb.com.br" target="_blank" class="underline hover:text-green-500"
+              >Ricardo Belfort</a
+            >
+          </div>
+        </footer>
+      </ng-container>
     </div>
   `,
 })
 export class AppComponent {
+  isPublicPage = false;
   currentYear = new Date().getFullYear();
   public APP_VERSION = APP_VERSION;
+
+  publicPages = ['/auth/login', '/auth/signup', '/auth/recovery'];
 
   constructor(
     private primeng: PrimeNG,
@@ -45,6 +60,10 @@ export class AppComponent {
 
   async ngOnInit() {
     this.primeng.ripple.set(true);
+
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+      this.isPublicPage = this.publicPages.some((path) => this.router.url.startsWith(path));
+    });
 
     const { data, error } = await supabase.auth.getSession();
 
