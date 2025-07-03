70aaacc55eacd4af29d8f9b25f040faaff50ab84
Ricardo Belfort <ricardo.belfort.magnotech@gmgroupbrasil.com.br>
2025-07-03 12:15:15 -0400

    fix: removendo keys

--- supabase.client.ts
+++ supabase.client.ts
@@ -1,7 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
+import { environment } from 'src/environments/environment';
 
-export const supabase = createClient(
-  'https://nqaipmnlcoioqqqzcghu.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xYWlwbW5sY29pb3FxcXpjZ2h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzMjc0MTAsImV4cCI6MjA2NjkwMzQxMH0.iG79kCCWM9C0qvKMx198kjdnWL4YkwPJAEk-sPneGRM'
-);
-// Pegue a URL e a chave no painel do Supabase (Project settings > API)
+export const supabase = createClient(environment.SUPABASE_URL, environment.SUPABASE_ANON_KEY);
